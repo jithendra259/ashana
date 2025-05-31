4f331a9b992a38e639e0f6d1a83557b36636d6fc
jithendra259 <jithendrasubramanyam218@gmail.com>
2025-05-31 12:37:42 +0530

    middleware.ts is add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
 
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-       
+       <p>footer</p>
       </footer>
     </div>
   );
