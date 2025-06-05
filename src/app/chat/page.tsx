31168c466ab2ff9e4b6adaa1afabeb300b30ad80
jithendra259 <jithendrasubramanyam218@gmail.com>
2025-06-05 20:36:27 +0530

    send button position fix

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import SendIcon from '@mui/icons-material/Send';
 import { Settings,Bot } from "lucide-react";
-import { SignInButton,SignOutButton, UserButton,SignUpButton,SignedOut,SignedIn} from "@clerk/nextjs"
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import { Button } from "@/components/ui/button";
-import ButtonBase from "@mui/material/ButtonBase";
+
 import React, { useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
+import { GlobeIcon,PlusIcon } from "lucide-react";
+import ButtonBase from '@mui/material/ButtonBase';
 export default function Chat({
   children,
 }: {
@@ -16,15 +19,24 @@ export default function Chat({
   <div className="flex flex-col h-full">
     <div className="flex-1"></div>
     <div className="p-4 w-full">
-      <div className="bg-zinc-800 rounded-2xl mx-auto p-4 w-[clamp(320px,60vw,700px)] min-w-[320px] max-w-[700px]">
+      <div className="bg-zinc-800 rounded-2xl mx-auto p-3 w-[clamp(320px,60vw,700px)] min-w-[320px] max-w-[700px]">
         <Textarea placeholder="Do you have doubt?" className="w-full" />
-        <div>
-          <Button className="mt-2" variant="outline">
-            Send
+        <div className="flex mt-2 grid-cols-1 md:grid-cols-2 gap-3">
+          <div>
+          <Button className="border-none rounded-full h-8 w-8" variant="outline">
+            <AddCircleOutlineIcon className=" "/>
           </Button>
-          <Button className="mt-2 ms-2" variant="outline">
-            Clear
+          </div>
+          <div>
+          <Button className="rounded-3xl" variant="outline">
+            <GlobeIcon className="" /><a>web</a>
           </Button>
+          </div>
+          <div className='ml-auto'>
+            <ButtonBase className=' text-white  ' title="Send">
+              <SendIcon className="" />
+            </ButtonBase>
+          </div>
         </div>
       </div>
     </div>
